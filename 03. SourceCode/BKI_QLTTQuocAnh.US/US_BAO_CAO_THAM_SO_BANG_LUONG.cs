744aea8af9ee7ae0e27b616fbaef7505bfce657b
hongminhbka <hongminhbka@gmail.com>
2016-01-08 15:52:44 +0700

    Thêm US, DS BAO CAO THAM SO BANG LUONG

---
+++ 03. SourceCode/BKI_QLTTQuocAnh.US/US_BAO_CAO_THAM_SO_BANG_LUONG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IP.Core.IPUserService;
+using BKI_DichVuMatDat.DS;
+
+namespace BKI_DichVuMatDat.US
+{
+    public class US_BAO_CAO_THAM_SO_BANG_LUONG: US_Object
+    {
+        public void GetThamSoBangLuongThang(DS_BAO_CAO_THAM_SO_BANG_LUONG ip_ds, decimal ip_dec_thang, decimal ip_dec_nam)
+        {
+            CStoredProc v_sp = new CStoredProc("PR_GET_THAM_SO_BANG_LUONG");
+            v_sp.addDecimalInputParam("@THANG", ip_dec_thang);
+            v_sp.addDecimalInputParam("@NAM", ip_dec_nam);
+            v_sp.fillDataSetByCommand(this, ip_ds);
+        }
+    }
+}
